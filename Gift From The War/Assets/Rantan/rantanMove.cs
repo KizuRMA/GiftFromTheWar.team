a5b8414abe1e8979bf12a882eee0832947d13ea9
AndoMutsuki <kd1333170@st.kobedenshi.ac.jp>
2022-05-09 10:38:08 +0900

    ver.0.0.1

--- Gift From The War/Assets/Rantan/rantanMove.cs
+++ Gift From The War/Assets/Rantan/rantanMove.cs
@@ -4,38 +4,104 @@ using UnityEngine;
 
 public class rantanMove : MonoBehaviour
 {
-    Transform rantanTransform;
-    int time;
-    int timeMax;
-    int hugou;
-    float move;
+    private Transform trans;
+    private FPSController fpsC;
+    private Transform camTrans;
+    private Quaternion firstQua;
+    private int upDown = -1;
+    [SerializeField] private Vector3 firstPos;
+    private float posY = 0;
+    [SerializeField] private float upDownSpeed;
+    [SerializeField] private float maxPosY;
+    [SerializeField] private float dashRaito;
+    [SerializeField] private float movePower = 0.0001f;
+    [SerializeField] private float upRaito = 0; //è„ÇÃåXÇ´ÇÃï‚ê≥î{ó¶
+    [SerializeField] private float downRaito = 0; //è„ÇÃåXÇ´ÇÃï‚ê≥î{ó¶
 
     // Start is called before the first frame update
     void Start()
     {
-        rantanTransform = GetComponent<Transform>();
-        time = 450;
-        timeMax = 900;
-        hugou = 1;
-        move = 0.0001f;
+        trans = GetComponent<Transform>();
+        camTrans = GameObject.Find("Main Camera").GetComponent<Transform>();
+        fpsC = GameObject.Find("player").GetComponent<FPSController>();
+        firstQua = trans.localRotation;
+        firstPos = trans.localPosition;
+        firstPos.y = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        time += hugou;
+        rotation();
+        tremor();
+    }
 
-        if(time > timeMax || time < 0)
+    void rotation()
+    {
+        Quaternion _ranQua = Quaternion.identity;
+
+        //ÉJÉÅÉâÇÃÉNÉIÅ[É^ÉjÉIÉìílÇéÊìæ
+        Quaternion _camQua = camTrans.rotation;
+
+        _ranQua = trans.rotation;
+
+        float _camEulerAngleX = _camQua.eulerAngles.x;
+
+        //äpìxÇí≤êÆÇ∑ÇÈ
+        if (_camEulerAngleX >= 300.0f)
         {
-            hugou *= -1;
-        }
-        if (hugou > 0)
-        {
-            rantanTransform.position += new Vector3(0, move, 0);
+            _camEulerAngleX -= 360.0f;
+            _camEulerAngleX *= upRaito;
         }
         else
         {
-            rantanTransform.position += new Vector3(0, -move, 0);
+            _camEulerAngleX *= downRaito;
         }
+
+        _camEulerAngleX *= -1;
+
+        _ranQua = Quaternion.AngleAxis(_camEulerAngleX, Vector3.right);
+        trans.localRotation = _ranQua * firstQua;
+    }
+
+    void tremor()
+    {
+        if (fpsC.GetMoveFlg())
+        {
+            if (fpsC.GetDashFlg())
+            {
+                posY += upDownSpeed * upDown * dashRaito;
+            }
+            else
+            {
+                posY += upDownSpeed * upDown;
+            }
+
+            if (Mathf.Abs(posY) > Mathf.Abs(maxPosY))
+            {
+                upDown *= -1;
+            }
+        }
+        else
+        {
+            if (Mathf.Abs(posY) > upDownSpeed * 1.1f)
+            {
+                if (posY > 0)
+                {
+                    posY += -upDownSpeed;
+                }
+                else
+                {
+                    posY += upDownSpeed;
+                }
+            }
+            else
+            {
+                posY = 0;
+                upDown = -1;
+            }
+        }
+
+        //trans.localPosition = firstPos + new Vector3(0.0f, posY, 0.0f);
     }
 }
