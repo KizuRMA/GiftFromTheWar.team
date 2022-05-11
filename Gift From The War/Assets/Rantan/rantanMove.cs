83e260dcce03f071498c1c26a536bf97bbd46999
AndoMutsuki <kd1333170@st.kobedenshi.ac.jp>
2022-05-11 13:11:45 +0900

    新しいベース

--- Gift From The War/Assets/Rantan/rantanMove.cs
+++ Gift From The War/Assets/Rantan/rantanMove.cs
@@ -4,38 +4,100 @@ using UnityEngine;
 
 public class rantanMove : MonoBehaviour
 {
-    Transform rantanTransform;
-    int time;
-    int timeMax;
-    int hugou;
-    float move;
+    private Transform trans;
+    private FPSController fpsC;
+    private Transform camTrans;
+    private Quaternion firstQua;
+    private int upDown = -1;
+    private Vector3 firstPos;
+    private float posY = 0;
+    [SerializeField] private float upDownSpeed;
+    [SerializeField] private float maxPosY;
+    [SerializeField] private float dashRaito;
+    [SerializeField] private float upRaito = 0; //上の傾きの補正倍率
+    [SerializeField] private float downRaito = 0; //上の傾きの補正倍率
 
     // Start is called before the first frame update
     void Start()
     {
-        rantanTransform = GetComponent<Transform>();
-        time = 450;
-        timeMax = 900;
-        hugou = 1;
-        move = 0.0001f;
+        trans = GetComponent<Transform>();
+        camTrans = GameObject.Find("Main Camera").GetComponent<Transform>();
+        fpsC = GameObject.Find("player").GetComponent<FPSController>();
+        firstQua = trans.localRotation;
+        firstPos = trans.localPosition;
     }
 
     // Update is called once per frame
     void Update()
     {
-        time += hugou;
+        rotation();
+        tremor();
+    }
 
-        if(time > timeMax || time < 0)
+    void rotation()
+    {
+
+        //カメラのクオータニオン値を取得
+        Quaternion _camQua = camTrans.rotation;
+
+        float _camEulerAngleX = _camQua.eulerAngles.x;
+
+        //角度を調整する
+        if (_camEulerAngleX >= 300.0f)
         {
-            hugou *= -1;
-        }
-        if (hugou > 0)
-        {
-            rantanTransform.position += new Vector3(0, move, 0);
+            _camEulerAngleX -= 360.0f;
+            _camEulerAngleX *= upRaito;
         }
         else
         {
-            rantanTransform.position += new Vector3(0, -move, 0);
+            _camEulerAngleX *= downRaito;
         }
+
+        _camEulerAngleX *= -1;
+
+        Quaternion _ranQua = Quaternion.AngleAxis(_camEulerAngleX, Vector3.right);
+        trans.localRotation = _ranQua * firstQua;
+    }
+
+    void tremor()
+    {
+        if (fpsC.GetMoveFlg())  //プレイヤーが動いているか
+        {
+            if (Mathf.Abs(trans.localPosition.y - firstPos.y) > Mathf.Abs(maxPosY))   //上下の移動のチェンジ
+            {
+                upDown *= -1;
+            }
+
+            if (fpsC.GetDashFlg())  //プレイヤーが走っているか
+            {
+                posY = upDownSpeed * upDown * dashRaito;
+            }
+            else
+            {
+                posY = upDownSpeed * upDown;
+            }
+        }
+        else
+        {
+            //自動で戻る処理
+            float nowPos = firstPos.y - trans.localPosition.y;
+            if (Mathf.Abs(nowPos) > upDownSpeed) //ほぼ最初のところに戻っていなかったら
+            {
+                if (nowPos > 0)
+                {
+                    posY = upDownSpeed;
+                }
+                else
+                {
+                    posY = -upDownSpeed;
+                }
+            }
+            else
+            {
+                posY = nowPos;
+                upDown = -1;
+            }
+        }
+        trans.localPosition += new Vector3(0.0f, posY, 0.0f);
     }
 }
---
+++ Gift From The War/Assets/Rantan/rantanWallTouch.cs
@@ -0,0 +1,109 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rantanWallTouch : MonoBehaviour
+{
+    Transform trans;
+    [SerializeField] float force;
+    Vector3 firstPos;
+    Rigidbody rd;
+    bool returnFlg = true;
+    [SerializeField] float firstSpeed;
+    [SerializeField] float speedRaito;
+    float nowSpeed;
+    Vector3 forceVec;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        trans = transform;
+        rd = this.GetComponent<Rigidbody>();
+        firstPos = transform.localPosition;
+    }
+
+
+
+
+    /// <summary>
+    /// アップデート
+    /// </summary>
+    void Update()
+    {
+        EraseInertia();
+
+        //押し戻す処理
+        if (returnFlg)
+        {
+            Return();
+        }
+    }
+
+    private void EraseInertia() //慣性を消す
+    {
+        rd.velocity = Vector3.zero;
+        rd.angularVelocity = Vector3.zero;
+    }
+
+    private void Return() //戻す処理
+    {
+        ReturnSpeedUp();
+
+        CalculateForceVec();
+
+        ForceVecCorrection();
+
+        trans.localPosition += forceVec;
+    }
+
+    private void ReturnSpeedUp() //戻す速さを速くする
+    {
+        nowSpeed *= speedRaito;
+    }
+
+    private void CalculateForceVec() //forceVecの計算  
+    {
+        forceVec = firstPos - trans.localPosition;
+    }
+
+    private void ForceVecCorrection() //ぶるぶるしないための補正
+    {
+        bool froceVecSmallFlg = forceVec.magnitude > nowSpeed;  //forceVecが小さすぎないか見る
+
+        if (froceVecSmallFlg)
+        {
+            forceVec = forceVec.normalized * nowSpeed;
+        }
+        else
+        {
+            nowSpeed = firstSpeed;
+            returnFlg = false;
+        }
+    }
+
+
+
+
+    /// <summary>
+    /// 衝突
+    /// </summary>
+    /// <param name="collison"></param>
+    private void OnCollisionEnter(Collision collison)
+    {
+        returnFlg = false;
+        nowSpeed = firstSpeed;
+    }
+
+
+
+
+
+    /// <summary>
+    /// 衝突終了
+    /// </summary>
+    /// <param name="collison"></param>
+    private void OnCollisionExit(Collision collison)
+    {
+        returnFlg = true;
+    }
+}
