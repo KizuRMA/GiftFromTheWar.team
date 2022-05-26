5ae7317857dadcb8762a8353df5e3c7eac4bebc5
KizuRMA <kd1309678@st.kobedenshi.ac.jp>
2022-05-26 17:07:46 +0900

    犬のナビゲーションを生成して、動かせるように調整

--- Gift From The War/Assets/enemy/dog/script/DogMoveState.cs
+++ Gift From The War/Assets/enemy/dog/script/DogMoveState.cs
@@ -5,18 +5,30 @@ using UnityEngine.AI;
 
 public class DogMoveState : MonoBehaviour
 {
+    [SerializeField]WayPoint wayPoint;
     [SerializeField]GameObject player;
+    [SerializeField]float Speed;
     NavMeshAgent agent;
+    int nextIndex;
 
     // Start is called before the first frame update
     void Start()
     {
+        nextIndex = 0;
         agent = GetComponent<NavMeshAgent>();
+        agent.speed = Speed;
     }
 
     // Update is called once per frame
     void Update()
     {
+       StartCoroutine(TargetCoroutine());
+        //agent.destination = player.transform.position;
+    }
+
+    private IEnumerator TargetCoroutine()
+    {
+        yield return new WaitForSeconds(3.0f);
         agent.destination = player.transform.position;
     }
 }
