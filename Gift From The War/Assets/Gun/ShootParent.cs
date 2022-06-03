8052ae1a2e8a45ddcaf8d39cd88959c3fa83a3d5
AndoMutsuki <kd1333170@st.kobedenshi.ac.jp>
2022-06-03 13:02:10 +0900

    クラス継承実験中

---
+++ Gift From The War/Assets/Gun/ShootParent.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootParent : MonoBehaviour
+{
+    //ゲームオブジェクトやスクリプト
+    protected Transform trans;
+    [SerializeField] protected Transform camTrans;
+    [SerializeField] protected remainingAmount energyAmount;
+    [SerializeField] protected GameObject bulletPrefab;
+    [SerializeField] protected GameObject bulletEffectPrefab;
+
+    //弾の発射  
+    [SerializeField] protected float shotSpeed;   //発射スピード
+    [SerializeField] protected float range;       //弾の消えるまでの時間
+    [SerializeField] protected float useEnergy;   //消費エネルギー
+
+    private List<GameObject> bullet = new List<GameObject>();   //弾の配列
+    private List<GameObject> bulletEffect = new List<GameObject>();   //弾の配列
+    protected Vector3 shotPos;                    //着弾点
+
+    protected void CreateBullet() //プレハブから弾を作る
+    {
+        //リストに弾を追加
+        bullet.Add((GameObject)Instantiate(bulletPrefab, trans.position, Quaternion.identity));
+
+        //目的地に球を方向転換
+        bullet[bullet.Count - 1].transform.LookAt(shotPos);
+
+        //射撃されてから指定秒後に銃弾のオブジェクトを破壊する
+        Destroy(bullet[bullet.Count - 1], range);
+
+
+        //リストに弾を追加
+        bulletEffect.Add((GameObject)Instantiate(bulletEffectPrefab, trans.position, Quaternion.identity));
+
+        //目的地に球を方向転換
+        bulletEffect[bulletEffect.Count - 1].transform.LookAt(shotPos);
+
+        //射撃されてから指定秒後に銃弾のオブジェクトを破壊する
+        Destroy(bulletEffect[bulletEffect.Count - 1], range);
+    }
+
+    protected void MoveBullet()   //弾の移動
+    {
+        if (bullet == null) return; //弾がなければ処理しない
+
+        for (int i = 0; i < bullet.Count; i++) //弾の数だけ繰り返す
+        {
+            if (bullet[i] == null)   //弾が破壊されていたら、リストから削除
+            {
+                bullet.RemoveAt(i);
+                bulletEffect.RemoveAt(i);
+                continue;
+            }
+            bullet[i].transform.transform.position += bullet[i].transform.forward * shotSpeed * Time.deltaTime; //移動処理
+            bulletEffect[i].transform.transform.position += bulletEffect[i].transform.forward * shotSpeed * Time.deltaTime; //移動処理
+        }
+    }
+}
--- Gift From The War/Assets/Gun/shooting.cs
+++ Gift From The War/Assets/Gun/shooting.cs
@@ -1,25 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
-public class shooting : MonoBehaviour
+#if UNITY_EDITOR
+[CustomEditor(typeof(ShootParent))]
+#endif
+
+public class shooting : ShootParent
 {
-    //ゲームオブジェクトやスクリプト
-    private Transform trans;
-    [SerializeField] private Transform camTrans;
-    [SerializeField] private GameObject bulletPrefab;
-    [SerializeField] private GameObject bulletEffectPrefab;
-    [SerializeField] private remainingAmount energyAmount;
-
-    //弾の発射
-    private List<GameObject> bullet = new List<GameObject>();   //弾の配列
-    private List<GameObject> bulletEffect = new List<GameObject>();   //弾の配列
-    [SerializeField] private float shotSpeed;   //発射スピード
-    [SerializeField] private float range;       //弾の消えるまでの時間
-    [SerializeField] private float useEnergy;   //消費エネルギー
+    //弾の発射  
     private bool shotFlg;                       //発射可能
     private Quaternion bulletQua;               //発射する弾の向き
-    private Vector3 shotPos;                    //着弾点
 
     private void Start()
     {
@@ -60,46 +52,6 @@ public class shooting : MonoBehaviour
         if (Physics.Raycast(ray, out hit, Mathf.Infinity))
         {
             shotPos = hit.point;
-        }
-    }
-
-    private void CreateBullet() //プレハブから弾を作る
-    {
-        //リストに弾を追加
-        bullet.Add((GameObject)Instantiate(bulletPrefab, trans.position, Quaternion.identity));
-
-        //目的地に球を方向転換
-        bullet[bullet.Count - 1].transform.LookAt(shotPos);
-
-        //射撃されてから指定秒後に銃弾のオブジェクトを破壊する
-        Destroy(bullet[bullet.Count - 1], range);
-
-
-        //リストに弾を追加
-        bulletEffect.Add((GameObject)Instantiate(bulletEffectPrefab, trans.position, Quaternion.identity));
-
-        //目的地に球を方向転換
-        bulletEffect[bulletEffect.Count - 1].transform.LookAt(shotPos);
-
-        //射撃されてから指定秒後に銃弾のオブジェクトを破壊する
-        Destroy(bulletEffect[bulletEffect.Count - 1], range);
-    }
-
-    private void MoveBullet()   //弾の移動
-    {
-        if (bullet == null) return; //弾がなければ処理しない
-
-        for (int i = 0; i < bullet.Count; i++) //弾の数だけ繰り返す
-        {
-            if (bullet[i] == null)   //弾が破壊されていたら、リストから削除
-            {
-                bullet.RemoveAt(i);
-                bulletEffect.RemoveAt(i);
-                continue;
-            }
-            bullet[i].transform.transform.position += bullet[i].transform.forward * shotSpeed * Time.deltaTime; //移動処理
-            bulletEffect[i].transform.transform.position += bulletEffect[i].transform.forward * shotSpeed * Time.deltaTime; //移動処理
-
         }
     }
 }
