4267f787504a81f941e0441030c4162a0835968a
nenemuke <nenemuke.1412@gmail.com>
2022-07-11 14:03:03 +0900

    ボタン作成（セレクト画面で導入確認）、終了確認パネルにアニメーションを実装

---
+++ Gift From The War/Assets/Title/Animation/ButtonReactionAnime.cs
@@ -0,0 +1,33 @@
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class ButtonReactionAnime : UIBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    [SerializeField] private Image ReactionImage;
+    
+
+   
+    protected override void Start()
+    {
+        base.Start();
+        
+    }
+
+    //マウスがボタンの上に来たら
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        ReactionImage.DOFade(endValue: 1f, duration: 0.25f).SetEase(Ease.OutCubic).Play();
+
+    }
+
+    //マウスがボタンにかぶっていなかったら
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        ReactionImage.DOFade(endValue: 0f, duration: 0.25f).SetEase(Ease.OutCubic).Play();
+    }
+
+    
+}
