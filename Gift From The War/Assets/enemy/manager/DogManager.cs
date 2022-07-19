5605b1aa02d440980fbb62a78eb7d6c3765d9df1
KizuRMA <kd1309678@st.kobedenshi.ac.jp>
2022-07-19 20:22:27 +0900

    犬のNavmeshベイク方法を変更した

--- Gift From The War/Assets/enemy/manager/DogManager.cs
+++ Gift From The War/Assets/enemy/manager/DogManager.cs
@@ -40,7 +40,7 @@ public class DogManager : BaseEnemyManager
             if (_state == null) continue;
 
             //現在参照している敵が追跡状態である時
-            if (_state.IsCurrentState(e_DogState.Tracking) == true)
+            if (_state.IsCurrentState(e_DogState.Tracking) == true || _state.IsCurrentState(e_DogState.Search) == true)
             {
                 //追跡状態の敵を管理している配列に現在参照している敵が入っていない時
                 if (objects.Contains(_ChildObjects[i]) == false)
@@ -69,20 +69,20 @@ public class DogManager : BaseEnemyManager
             }
         }
 
-        //管理している配列ないに追跡していない敵がいる場合
-        for (int i = 0; i < objects.Count; i++)
-        {
-            DogState _state = objects[i].GetComponent<DogState>();
-            if (_state == null) continue;
+        ////管理している配列ないに追跡していない敵がいる場合
+        //for (int i = 0; i < objects.Count; i++)
+        //{
+        //    DogState _state = objects[i].GetComponent<DogState>();
+        //    if (_state == null) continue;
 
-            //現在参照している敵が追跡状態でない時
-            if (_state.IsCurrentState(e_DogState.Tracking) == false)
-            {
-                //エージェントタイプを変更して配列から削除する
-                objects[i].GetComponent<NavMeshAgent>().agentTypeID = agentTypeIdDict["DogAgent"];
-                objects.Remove(objects[i]);
-            }
-        }
+        //    //現在参照している敵が追跡状態でない時
+        //    if (_state.IsCurrentState(e_DogState.Tracking) == false)
+        //    {
+        //        //エージェントタイプを変更して配列から削除する
+        //        objects[i].GetComponent<NavMeshAgent>().agentTypeID = agentTypeIdDict["DogAgent"];
+        //        objects.Remove(objects[i]);
+        //    }
+        //}
     }
 
     public void ResetPriority()
