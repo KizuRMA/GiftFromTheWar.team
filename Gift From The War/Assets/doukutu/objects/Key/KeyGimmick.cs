ca8e46f9e4844900ad1617cb8126f2c4762af2ac
KizuRMA <kd1309678@st.kobedenshi.ac.jp>
2022-09-07 14:03:14 +0900

    ドアのギミック完成

---
+++ Gift From The War/Assets/doukutu/objects/Key/KeyGimmick.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyGimmick : MonoBehaviour
+{
+    [SerializeField] private GameObject rightDoor;
+    [SerializeField] private GameObject leftDoor;
+    [SerializeField] private GameObject playerKeyObj;
+    [SerializeField] private GameObject keyObj;
+
+    private bool openFlg;
+
+    [SerializeField] private float movePower;
+    [SerializeField] private float maxPos;
+    private float nowPower;
+    private float sumPos;
+    private bool switchFlg = false;
+
+    private void Awake()
+    {
+
+    }
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        nowPower = 0;
+
+        if (openFlg)
+        {
+            OpneDoor();
+        }
+        else
+        {
+            CloseDoor();
+        }
+
+        sumPos += nowPower;
+        rightDoor.transform.localPosition += new Vector3(nowPower, 0, 0);
+        leftDoor.transform.localPosition += new Vector3(-nowPower, 0, 0);
+    }
+
+    private void OpneDoor()
+    {
+        if (sumPos >= maxPos) return;
+        nowPower = movePower * Time.deltaTime;
+
+        //âπÇçƒê∂
+        if (!switchFlg)
+        {
+            AudioManager.Instance.PlaySE("BallPlate", gameObject, isLoop: false);
+            switchFlg = true;
+        }
+
+        AudioManager.Instance.PlaySE("OpenDoor1", gameObject, isLoop: false);
+
+    }
+
+    private void CloseDoor()
+    {
+        if (sumPos <= 0) return;
+        nowPower = -movePower * Time.deltaTime;
+
+        //âπÇçƒê∂
+        AudioManager.Instance.PlaySE("OpenDoor1", gameObject, isLoop: false);
+        switchFlg = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (playerKeyObj == null || keyObj == null || other.gameObject.tag != "Player") return;
+
+        if (playerKeyObj.activeSelf == true)
+        {
+            openFlg = true;
+            playerKeyObj.SetActive(false);
+            keyObj.SetActive(true);
+
+        }
+    }
+}
