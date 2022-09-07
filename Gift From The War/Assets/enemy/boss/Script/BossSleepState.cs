82f0c00fee932650d412275205011a44880a18fa
KizuRMA <kd1309678@st.kobedenshi.ac.jp>
2022-09-07 11:22:55 +0900

    鍵のプレハブ作成、ドアの配置、鍵の配置、ボスの音追加

--- Gift From The War/Assets/enemy/boss/Script/BossSleepState.cs
+++ Gift From The War/Assets/enemy/boss/Script/BossSleepState.cs
@@ -5,20 +5,32 @@ using UnityEngine;
 public class BossSleepState : State<BossState>
 {
     public BossSleepState(BossState owner) : base(owner) { }
+    float voiceTimer;
 
     public override void Enter()
     {
         owner.agent.isStopped = true;
         owner.agent.updatePosition = false;
         owner.agent.updateRotation = false;
+        voiceTimer = 0;
     }
 
     public override void Execute()
     {
         if (owner.getupFlg == true)
         {
+            AudioManager.Instance.PlaySE("BossGetUpVoice", owner.transform.gameObject,vol:2.0f);
             owner.animator.SetTrigger("GetUp");
-            owner.animator.SetFloat("Speed", 0.6f);
+            owner.animator.SetFloat("Speed", 0.4f);
+        }
+        else
+        {
+            voiceTimer += Time.deltaTime;
+            if (voiceTimer >= 12.0f)
+            {
+                AudioManager.Instance.PlaySE("BossSleepVoice", owner.transform.gameObject,vol:0.5f);
+                voiceTimer = 0;
+            }
         }
 
         //起きるアニメーションに切り替わったとき
