4885e3bc16cc2f9e515df34407ac72cf6ac3801d
AndoMutsuki <kd1333170@st.kobedenshi.ac.jp>
2022-09-11 19:38:14 +0900

    タイムアタックのバグ修正

---
+++ Gift From The War/Assets/TimeAttack/TimeAttackCursor.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeAttackCursor : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        CursorManager.Instance.cursorLock = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
